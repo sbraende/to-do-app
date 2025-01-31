7f6f593d28851fd67dbcdb9a6b81a3f7a04e0537
sbraende <sebastian.braende@gmail.com>
2025-01-31 13:15:16 +0100

    Create listData.js with dummy data

---
+++ src/js/listData.js
@@ -0,0 +1,17 @@
+const listItems = [
+  {
+    task: "Create design",
+    date: "31.01.2025",
+    done: false,
+  },
+  {
+    task: "Implement Frontend",
+    date: "21.02.2025",
+    done: false,
+  },
+  {
+    task: "Implement Backend",
+    date: "25.02.2025",
+    done: false,
+  },
+];
